8d9da83172ca946b2a6fb6adf8b74224546bcada
Leo Afonso <leandro.sant.afonso@gmail.com>
2019-05-03 07:22:20 +0100

    initial commit

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
+const DB_URL = process.env.DB_URL
+app.listen(3000, ()=> {
+  console.log(`app is running on port ${DB_URL}`);
 })
+
+console.log(DB_URL)
