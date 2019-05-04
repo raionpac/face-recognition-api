b0e7b412e357f8c50f86764b9d6cc6fa3915aad2
Leo Afonso <leandro.sant.afonso@gmail.com>
2019-05-04 07:13:58 +0100

    port var

--- server.js
+++ server.js
@@ -29,8 +29,9 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
+const port = process.env.PORT || 3000
+app.listen(port, ()=> {
+  console.log(`app is running on port ${port}`);
 })
 
-//console.log(process.env.PORT)
+//console.log(port)
