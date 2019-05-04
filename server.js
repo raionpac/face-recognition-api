93fc6081127effe76b99f57d94a45af4964a52c6
Leo Afonso <leandro.sant.afonso@gmail.com>
2019-05-04 08:28:07 +0100

    update

--- server.js
+++ server.js
@@ -19,6 +19,19 @@ const db = knex({
 
 const app = express();
 
+app.get('/db', async (req, res) => {
+  try {
+    const client = await db.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 app.use(cors())
 app.use(bodyParser.json());
 
